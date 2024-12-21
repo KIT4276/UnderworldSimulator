324f6e6218aeda6517a02836c5ecae807140b65f
KIT4276 <82262901+KIT4276@users.noreply.github.com>
2024-12-21 14:06:48 +0300

    fix and  simplified ObstacleTransparent

--- Assets/Scripts/Triggers/Obstacles/ObstacleTransparent.cs
+++ Assets/Scripts/Triggers/Obstacles/ObstacleTransparent.cs
@@ -1,12 +1,10 @@
-using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 
 public class ObstacleTransparent : BaseObstacle
 {
-    [SerializeField] private AnimationCurve _curve;
-    [Space]
     [SerializeField] private float _transparencyValue = 0.5f;
-    private float _transparencyTime = 1.0f;
+    [SerializeField] private float _transparencyTime = 1.0f;
 
     protected override void OnTriggerEnter2D(Collider2D collision)
     {
@@ -14,9 +12,10 @@ public class ObstacleTransparent : BaseObstacle
 
         if (CanFade())
         {
-            StartCoroutine(FadeIn());
+            _spriteRenderer.DOFade(_transparencyValue, _transparencyTime);
         }
     }
+
 
     protected override void OnTriggerExit2D(Collider2D collision)
     {
@@ -24,52 +23,8 @@ public class ObstacleTransparent : BaseObstacle
 
         if (this.gameObject.activeSelf && _hero != null)
         {
-            StartCoroutine(FadeOut());
+            _spriteRenderer.DOFade(1, _transparencyTime);
         }
-    }
-
-    private IEnumerator FadeOut()
-    {
-        float t = _transparencyTime;
-        var startAlpha = _spriteRenderer.color.a;
-
-        while (t > 0f)
-        {
-            t -= Time.deltaTime;
-            var delta = (1 - _curve.Evaluate(t));
-            var a = startAlpha + delta;
-            _spriteRenderer.color =
-                new Color(_spriteRenderer.color.r, _spriteRenderer.color.g, _spriteRenderer.color.b, a);
-
-            if (a >= 1)
-                yield break;
-
-            yield return 0;
-        }
-        yield return null;
-    }
-
-    private IEnumerator FadeIn()
-    {
-        float t = _transparencyTime;
-        var startAlpha = _spriteRenderer.color.a;
-
-        while (t > 0f)
-        {
-            t -= Time.deltaTime;
-            var delta = (1- _curve.Evaluate(t));
-
-            float a = startAlpha - delta;
-
-            _spriteRenderer.color =
-                new Color(_spriteRenderer.color.r, _spriteRenderer.color.g, _spriteRenderer.color.b, a);
-
-            if (a <= _transparencyValue)
-                yield break;
-
-            yield return 0;
-        }
-        yield return null;
     }
 
     private bool CanFade() =>
