c5252d960f16d3eb67bcebeb93b254ed41357e65
KIT4276 <82262901+KIT4276@users.noreply.github.com>
2025-01-26 11:56:46 +0300

    clean up

--- Assets/Scripts/Decorating/Floor.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class Floor : MonoBehaviour
-{
-    [SerializeField] private PolygonCollider2D _polygonCollider;
-    public PolygonCollider2D Polygon {get => _polygonCollider; }
-}
