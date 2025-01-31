4b355f0d811c4fcfbe617174d6a48582c1a33738
KIT4276 <82262901+KIT4276@users.noreply.github.com>
2025-01-31 17:30:56 +0300

    Inventory canvas done

---
+++ Assets/Scripts/UI/FilterButton.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class FilterButton : MonoBehaviour
+{
+    [SerializeField] private GameObject _inactiveImage;
+    [SerializeField] private GameObject _activeImage;
+    [SerializeField] public bool _isActive;
+
+    public bool IsActive { get => _isActive; }
+
+    private void Start()
+    {
+        if (_isActive)
+            Activate();
+        else
+            Deactivate();
+    }
+
+    public void Switch()
+    {
+        if (_isActive)
+            Deactivate();
+        else
+            Activate();
+    }
+
+
+    public void Activate()
+    {
+        _isActive = true;
+        _inactiveImage.SetActive(false);
+        _activeImage.SetActive(true);
+    }
+
+    public void Deactivate()
+    {
+        _isActive = false;
+        _inactiveImage.SetActive(true);
+        _activeImage.SetActive(false);
+    }
+}
+
