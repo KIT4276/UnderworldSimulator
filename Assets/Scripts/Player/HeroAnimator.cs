61fa1da4a68fe4eb3d4073e6d39403b0e9015593
KIT4276 <82262901+KIT4276@users.noreply.github.com>
2025-02-04 21:59:41 +0300

    fix animations

--- Assets/Scripts/Player/HeroAnimator.cs
+++ Assets/Scripts/Player/HeroAnimator.cs
@@ -1,6 +1,4 @@
 using DragonBones;
-using System.Collections;
-using System.Timers;
 using UnityEngine;
 
 public class HeroAnimator : MonoBehaviour
@@ -11,10 +9,8 @@ public class HeroAnimator : MonoBehaviour
     [SerializeField] private UnityArmatureComponent _armatureRight;
     [SerializeField] private UnityArmatureComponent _armatureIdle;
     [SerializeField] private HeroMove _heroMove;
-    [SerializeField] private float _transitionTime = 0.2f;
 
     private UnityArmatureComponent _currentArmature;
-    private Vector2 _lastInput = Vector2.zero;
 
     private void Start()
     {
@@ -26,66 +22,36 @@ public class HeroAnimator : MonoBehaviour
     {
         if (_heroMove.InputVector2.x > 0)
         {
-            CrossfadeArmatures(_currentArmature, _armatureRight, _transitionTime);
+            CrossArmatures(_armatureRight);
+
         }
         else if (_heroMove.InputVector2.x < 0)
         {
-            CrossfadeArmatures(_currentArmature, _armatureLeft, _transitionTime);
+            CrossArmatures(_armatureLeft);
         }
         else
         {
             if (_heroMove.InputVector2.y > 0)
             {
-                CrossfadeArmatures(_currentArmature, _armatureBack, _transitionTime);
+                CrossArmatures(_armatureBack);
             }
             else if (_heroMove.InputVector2.y < 0)
             {
-                CrossfadeArmatures(_currentArmature, _armatureFront, _transitionTime);
+                CrossArmatures(_armatureFront);
             }
             else
             {
-                CrossfadeArmatures(_currentArmature, _armatureIdle, _transitionTime);
+                CrossArmatures(_armatureIdle);
             }
         }
     }
 
-    public void CrossfadeArmatures(UnityArmatureComponent oldArmature, UnityArmatureComponent newArmature, float duration)
+    public void CrossArmatures(UnityArmatureComponent newArmature)
     {
-        StartCoroutine(FadeArmatures(oldArmature, newArmature, duration));
-    }
+        if(_currentArmature == newArmature) return;
 
-    private IEnumerator FadeArmatures(UnityArmatureComponent oldArmature, UnityArmatureComponent newArmature, float duration)
-    {
-        float time = 0;
-        _currentArmature = newArmature;
-
+        _currentArmature.gameObject.SetActive(false);
         newArmature.gameObject.SetActive(true);
-        SetArmatureAlpha(newArmature, 0);
-
-        while (time < duration)
-        {
-            time += Time.deltaTime;
-            float alpha = time / duration;
-
-            SetArmatureAlpha(oldArmature, 1 - alpha);
-            SetArmatureAlpha(newArmature, alpha);
-
-            yield return null;
-        }
-
-        oldArmature.gameObject.SetActive(false);
-    }
-
-    private void SetArmatureAlpha(UnityArmatureComponent armature, float alpha)
-    {
-        foreach (Slot slot in armature.armature.GetSlots())
-        {
-            if (slot.display != null && slot.display is MeshRenderer meshRenderer)
-            {
-                Color color = meshRenderer.material.color;
-                color.a = alpha;
-                meshRenderer.material.color = color;
-            }
-        }
+        _currentArmature = newArmature;
     }
 }
