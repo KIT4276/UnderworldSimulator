45d1f07c2e69469ab0d9c96306131f1d1b1848cb
KIT4276 <82262901+KIT4276@users.noreply.github.com>
2025-02-04 10:27:56 +0300

    start

---
+++ Assets/Scripts/Editor/RandomPositioner.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+[RequireComponent (typeof(IRandomizedPosition))]
+public class RandomPositioner : MonoBehaviour
+{
+     private IRandomizedPosition _randomizedPosition;
+
+    [ContextMenu("Randomize Positions")]
+    public void RandomizePositions()
+    {
+        _randomizedPosition = GetComponent<IRandomizedPosition>();
+
+
+        if (_randomizedPosition.GetTransforms() == null || _randomizedPosition.GetTransforms().Length == 0)
+        {
+            Debug.LogWarning("No objects assigned to move!");
+            return;
+        }
+
+        foreach (Transform obj in _randomizedPosition.GetTransforms())
+        {
+            if (obj != null)
+            {
+                float randomX = Random.Range(_randomizedPosition.GetMinBounds().x, _randomizedPosition.GetMaxBounds().x);
+                float randomY = Random.Range(_randomizedPosition.GetMinBounds().y, _randomizedPosition.GetMaxBounds().y);
+                obj.position = new Vector3(randomX, randomY, 0);
+            }
+        }
+
+        Debug.Log("Objects repositioned successfully!");
+    }
+}
---
+++ Assets/Scripts/Editor/RandomPositionerEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(RandomPositioner))]
+public class RandomPositionerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        RandomPositioner script = (RandomPositioner)target;
+        if (GUILayout.Button("Randomize Positions"))
+        {
+            script.RandomizePositions();
+            EditorUtility.SetDirty(script); 
+        }
+    }
+}
