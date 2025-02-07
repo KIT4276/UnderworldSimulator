828cd76e54a24338d78baa05553aee06a4d55a5b
KIT <kit311290@gmail.com>
2025-02-07 19:48:46 +0300

    cells

---
+++ Assets/Scripts/Decorating/Cells.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+using Zenject;
+
+public class Cells : MonoBehaviour
+{
+    [SerializeField] private GameObject _cells;
+
+    
+    private DecorationState _decorationState;
+
+
+    [Inject]
+    public void Construct(DecorationState decorationState)
+    {
+        _cells.SetActive(false);
+        _decorationState = decorationState;
+        _decorationState.DecorationStateEnter += OnDecorStateEnter;
+        _decorationState.DecorationStateExit += OnDecorStateExit;
+    }
+
+    private void OnDecorStateExit()
+    {
+        _cells.SetActive(false);
+    }
+
+    private void OnDecorStateEnter()
+    {
+        _cells.SetActive(true);
+    }
+
+    private void OnDisable()
+    {
+        _decorationState.DecorationStateEnter -= OnDecorStateEnter;
+        _decorationState.DecorationStateExit -= OnDecorStateExit;
+    }
+}
