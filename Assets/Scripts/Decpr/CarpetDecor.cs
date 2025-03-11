32e59817c09c623cf512706e7cb831e8817bc0d4
KIT <kit311290@gmail.com>
2025-03-11 08:23:59 +0300

    done

---
+++ Assets/Scripts/Decpr/CarpetDecor.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[RequireComponent(typeof(DecorView), (typeof(DecorRotator)))]
+[RequireComponent(typeof(DecorDrag), (typeof(CarpetPlacer)))]
+public class CarpetDecor : Decor
+{
+   
+}
---
+++ Assets/Scripts/Decpr/CarpetPlacer.cs
@@ -0,0 +1,7 @@
+﻿public class CarpetPlacer : DecorPlacer
+{
+    protected override bool CheckOtherDecor(bool isInside)
+    {
+        return isInside;
+    }
+}
