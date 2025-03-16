c717ac0b8089cae3035bbaf6076bf231589664fc
KIT <kit311290@gmail.com>
2025-03-16 12:40:25 +0300

    TaskParameters

--- Assets/Scripts/ScriptableObjectsScripts/TasksData.cs
+++ Assets/Scripts/ScriptableObjectsScripts/TasksData.cs
@@ -1,14 +1,25 @@
+using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "TaskData", menuName = "ScriptableObjects/TaskData", order = 4)]
 
 public class TasksData : ScriptableObject
 {
+    [SerializeField] private TaskParameters[] _tasks;
+
+    public TaskParameters[] Tasks { get => _tasks; }
+}
+
+[Serializable]
+public class TaskParameters
+{
     [SerializeField] private string _name;
+    [SerializeField] private GuestsType _guestsType;
     [SerializeField] private RoomParameterType _parameterType;
     [SerializeField] private int _value;
 
-    public string Name{get => _name; }
+    public string Name { get => _name; }
+    public GuestsType GuestsType { get => _guestsType; }
     public RoomParameterType ParameterType { get => _parameterType; }
     public int Value { get => _value; }
 }
