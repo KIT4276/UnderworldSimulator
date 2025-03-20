4ed8eb583261e82374210e52901fb0b7a0dbb65b
KIT4276 <82262901+KIT4276@users.noreply.github.com>
2025-03-20 15:19:15 +0300

    Milestone test, Milestone and progress view

---
+++ Assets/Scripts/ForTests/Tests.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Zenject;
+
+public class Tests : MonoBehaviour
+{
+    [SerializeField] private InputActionReference _progressUpAction;
+    [Inject] private ProgressSystem _progressSystem;
+
+    private void Start()
+    {
+        _progressUpAction.action.performed += OnProgressUp;
+    }
+
+    private void OnProgressUp(InputAction.CallbackContext context)
+    {
+            _progressSystem.UpProgress(10);
+    }
+
+    private void OnDestroy()
+    {
+        _progressUpAction.action.performed -= OnProgressUp;
+    }
+}
