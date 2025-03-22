85f85ee2f161600c02156ccd7bf1c5b3e2db50a9
KIT <kit311290@gmail.com>
2025-03-22 14:03:04 +0300

    quest filter removed

--- Assets/Scripts/Loot/LootInteract.cs
+++ Assets/Scripts/Loot/LootInteract.cs
@@ -9,7 +9,7 @@ public class LootInteract : InteractableObstacle
     [SerializeField] protected GameObject _progressBar;
     [SerializeField] protected Image _bar;
     [SerializeField] protected CraftLoot _craftLoot;
-    [SerializeField] protected QuestsLoot _questsLoot;
+   // [SerializeField] protected QuestsLoot _questsLoot;
     [SerializeField] protected GameObject _sprite;
     [SerializeField] private Collider2D _interactableCollider;
     [SerializeField] private GoParticleSystem _goParticleSystem;
@@ -67,10 +67,10 @@ public class LootInteract : InteractableObstacle
             {
                 FillLoot(lootSetting);
             }
-            foreach (var lootSetting in _questsLoot.LootSettings)
-            {
-                FillLoot(lootSetting);
-            }
+            //foreach (var lootSetting in _questsLoot.LootSettings)
+            //{
+            //    FillLoot(lootSetting);
+            //}
             _IsFilled = true;
         }
     }
