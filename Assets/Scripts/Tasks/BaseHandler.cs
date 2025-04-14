0fce38c50883793036f092a52cb06d8c11b804aa
KIT4276 <82262901+KIT4276@users.noreply.github.com>
2025-04-14 14:50:11 +0300

    fix: duplication of drawings when adding new ones

--- Assets/Scripts/Tasks/BaseHandler.cs
+++ Assets/Scripts/Tasks/BaseHandler.cs
@@ -37,12 +37,12 @@ public abstract class BaseHandler
         //Debug.Log(_all.Length);
         foreach (var item in _all)
         {
-            if (this is GuestsHandler)
-            {
-                Debug.Log(item.Name);
-                Debug.Log(item.MilestonesIndex);
-                Debug.Log(_milestoneSystem.CurrentMilestonesIndex() - 1);
-            }
+            //if (this is GuestsHandler)
+            //{
+            //    Debug.Log(item.Name);
+            //    Debug.Log(item.MilestonesIndex);
+            //    Debug.Log(_milestoneSystem.CurrentMilestonesIndex() - 1);
+            //}
 
             if (item.MilestonesIndex <= _milestoneSystem.CurrentMilestonesIndex() - 1)
             {
