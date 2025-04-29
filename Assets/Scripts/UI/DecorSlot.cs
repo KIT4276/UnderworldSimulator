26f27570eda23f07621696e9f15bd5ab1b2a0ed9
KIT <kit311290@gmail.com>
2025-04-29 18:37:31 +0300

    done: tabs

--- Assets/Scripts/UI/DecorSlot.cs
+++ Assets/Scripts/UI/DecorSlot.cs
@@ -1,4 +1,3 @@
-using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,39 +5,33 @@ using UnityEngine.UI;
 public class DecorSlot : MonoBehaviour
 {
     [SerializeField] private Image _image;
-    [SerializeField] private TMP_Text _paramName_1;
+    [SerializeField] private GameObject _background;
+    [Space]
+    [SerializeField] private Image _paramIcon_1;
     [SerializeField] private TMP_Text _paramValue_1;
-    [SerializeField] private TMP_Text _paramName_2;
+    [SerializeField] private Image _paramIcon_2;
     [SerializeField] private TMP_Text _paramValue_2;
-    [SerializeField] private TMP_Text _paramName_3;
+    [SerializeField] private Image _paramIcon_3;
     [SerializeField] private TMP_Text _paramValue_3;
 
     public void FillSlot(Decor decor, ParameterData parameterData)
     {
+        _background.SetActive(true);
         _image.gameObject.SetActive(true);
         _image.sprite = decor.GetIcon();
 
-        _paramName_1.text = parameterData.FindParamByType(decor.Parameters.Parameters[0].ParameterType).Name;
+        _paramIcon_1.sprite = parameterData.FindParamByType(decor.Parameters.Parameters[0].ParameterType).Icon;
         _paramValue_1.text = decor.Parameters.Parameters[0].Value.ToString();
 
-        _paramName_2.text = parameterData.FindParamByType(decor.Parameters.Parameters[1].ParameterType).Name;
+        _paramIcon_2.sprite = parameterData.FindParamByType(decor.Parameters.Parameters[1].ParameterType).Icon;
         _paramValue_2.text = decor.Parameters.Parameters[1].Value.ToString();
 
-        _paramName_3.text = parameterData.FindParamByType(decor.Parameters.Parameters[2].ParameterType).Name;
+        _paramIcon_3.sprite = parameterData.FindParamByType(decor.Parameters.Parameters[2].ParameterType).Icon;
         _paramValue_3.text = decor.Parameters.Parameters[2].Value.ToString();
     }
 
     public void FillEmpty()
     {
-        _image.gameObject.SetActive(false);
-
-        _paramName_1.text = string.Empty;
-        _paramValue_1.text = string.Empty;
-
-        _paramName_2.text = string.Empty;
-        _paramValue_2.text = string.Empty;
-
-        _paramName_3.text = string.Empty;
-        _paramValue_3.text = string.Empty;
+        _background.SetActive(false);
     }
 }
