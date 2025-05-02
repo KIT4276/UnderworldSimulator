1534c6b788b6265c87483ee375855a9f9d56e8b9
distant-earth <lobanovakristine@yandex.ru>
2025-05-02 23:16:43 +0300

    Starting tutorial created

---
+++ Assets/Scripts/UI/Tutorials/StartTutorial.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Zenject;
+
+public class StartTutorial : MonoBehaviour
+{
+    [SerializeField] private GameObject tutorialBox;
+    [SerializeField] private InputActionAsset inputActions; // Drag your .inputactions asset here
+    private InputAction skipAction;
+
+
+    private Animator animtut;
+    private StateMachine stateMachine;
+    private InputAction advanceAction;
+    private bool tutorialStarted = false;
+
+    [Inject]
+    public void Construct(StateMachine stateMachine)
+    {
+        this.stateMachine = stateMachine;
+        stateMachine.ChangeStateAction += OnStateChange;
+    }
+
+    private void Awake()
+    {
+        animtut = GetComponent<Animator>();
+
+        // Create input action to listen to Space bar
+        advanceAction = new InputAction(type: InputActionType.Button, binding: "<Keyboard>/space");
+        advanceAction.performed += ctx => ChangeAnimation();
+
+        // Create input action for Escape key (to skip tutorial)
+        skipAction = new InputAction(type: InputActionType.Button, binding: "<Keyboard>/escape");
+        skipAction.performed += ctx => SkipTutorial();  // Skip the tutorial when Escape is pressed
+    }
+
+    private void OnStateChange(IExitableState newState)
+    {
+        if (newState is GameLoopState && !tutorialStarted)
+        {
+            tutorialStarted = true;
+            tutorialBox.SetActive(true);
+            advanceAction.Enable();
+            skipAction.Enable();
+
+            // Disable all action maps
+            DisableAllInputMaps();
+
+            stateMachine.ChangeStateAction -= OnStateChange;
+        }
+    }
+
+    private void ChangeAnimation()
+    {
+        if (tutorialBox.activeSelf)
+        {
+            int currentStep = animtut.GetInteger("Change");
+            animtut.SetInteger("Change", currentStep + 1);
+        }
+    }
+
+    private void SkipTutorial()
+    {
+        Endtut();  // Skip the tutorial
+    }
+
+    public void Endtut()
+    {
+        animtut.enabled = false;
+        //tutorialBox.SetActive(false);
+        advanceAction.Disable();
+        skipAction.Disable();
+
+        // Re-enable main game input maps
+        EnableGameplayInputMaps();
+
+        gameObject.SetActive(false);
+    }
+
+    private void DisableAllInputMaps()
+    {
+        foreach (var map in inputActions.actionMaps)
+        {
+            map.Disable();
+        }
+    }
+
+    private void EnableGameplayInputMaps()
+    {
+        EnableMapIfExists("Player");
+        EnableMapIfExists("UI");
+        EnableMapIfExists("Map");
+        EnableMapIfExists("Help");
+    }
+
+    private void EnableMapIfExists(string mapName)
+    {
+        var map = inputActions.FindActionMap(mapName, true);
+        if (map != null)
+        {
+            map.Enable();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        advanceAction.Dispose();
+        skipAction.Dispose();
+        stateMachine.ChangeStateAction -= OnStateChange;
+    }
+}
