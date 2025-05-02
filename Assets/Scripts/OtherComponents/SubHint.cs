c748ab1d55f71ffb2e82e5dfa1d023471fc9c969
KIT <kit311290@gmail.com>
2025-05-02 18:48:35 +0300

    add: ToolKit for inventory slots

---
+++ Assets/Scripts/OtherComponents/SubHint.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubHint : MonoBehaviour
+{
+    [SerializeField] private Image _iconParam;
+    [SerializeField] private TMP_Text _valueParam;
+
+    public void Fill(Sprite icon, int value)
+    {
+        if (value > 0)
+        {
+            _iconParam.sprite = icon;
+            _valueParam.text = "+" + value.ToString();
+        }
+        else
+        {
+           this.gameObject.SetActive(false);
+        }
+    }
+}
