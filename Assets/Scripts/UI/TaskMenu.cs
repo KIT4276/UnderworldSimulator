2b327b2ad46c6156812aca5b2b8e784ef08cd1dd
KIT <kit311290@gmail.com>
2025-05-02 08:33:26 +0300

    fix: updating tasks after eviction; add: filter tasks - by selected room

--- Assets/Scripts/UI/TaskMenu.cs
+++ Assets/Scripts/UI/TaskMenu.cs
@@ -9,31 +9,39 @@ public class TaskMenu : MonoBehaviour
     private TasksHandler _tasksHandler;
     private bool _isInit;
     private StateMachine _stateMachine;
+    private RoomsSystem _roomsSystem;
 
     [Inject]
-    private void  Construct(TasksHandler tasksHandler, StateMachine stateMachine)
+    private void Construct(TasksHandler tasksHandler, StateMachine stateMachine, RoomsSystem roomsSystem)
     {
         _tasksHandler = tasksHandler;
         _stateMachine = stateMachine;
+        _roomsSystem = roomsSystem;
     }
 
     private void Start()
     {
         _stateMachine.ChangeStateAction += OnChangeState;
         _tasksHandler.AvailableUpdate += OnAvailableUpdate;
+        _roomsSystem.RoomSelected += OnRoomChanged;
+        _roomsSystem.RoomsParamsChanged += OnRoomChanged;
 
-        foreach(var task in _tasksHandler.All)
+        foreach (var task in _tasksHandler.All)
         {
             task.BecameAvailable += OnBecameAvailable;
         }
-        
-       
     }
 
     private void Awake()
     {
         Fill();
     }
+
+    private void OnRoomChanged(Room room)
+    {
+        Fill();
+    }
+
 
     private void OnBecameAvailable(BaseHandledReward reward)
     {
@@ -56,16 +64,23 @@ public class TaskMenu : MonoBehaviour
 
     private void Fill()
     {
-        for (int i = 0; i < _parametersSlots.Length; i++)
+        foreach (var slot in _parametersSlots)
         {
+            slot.FillEmpty();
+        }
 
-            if (_tasksHandler.AvailableList.Count >i)
+        int i = 0;
+
+        foreach (var reward in _tasksHandler.AvailableList)
+        {
+            if (reward is Task task)
             {
-                _parametersSlots[i].FillSlot((Task)_tasksHandler.AvailableList[i]);
-            }
-            else
-            {
-                _parametersSlots[i].FillEmpty();
+                if (_roomsSystem.SelectedRoom.Guest != null &&
+                task.GuestsType == _roomsSystem.SelectedRoom.Guest.Type)
+                {
+                    _parametersSlots[i].FillSlot(task);
+                    i++;
+                }
             }
         }
     }
@@ -74,6 +89,7 @@ public class TaskMenu : MonoBehaviour
     {
         _stateMachine.ChangeStateAction -= OnChangeState;
         _tasksHandler.AvailableUpdate -= OnAvailableUpdate;
+        _roomsSystem.RoomSelected -= OnRoomChanged;
         _tasksHandler.OnDestroy();
     }
 }
