0597334ffecedf28ee8a79659f8cf8fc81d8d018
KIT <kit311290@gmail.com>
2025-05-13 19:53:14 +0300

    add: unattainable milestone; hiding awards on it

--- Assets/Scripts/UI/ProgressBarSwitch.cs
+++ Assets/Scripts/UI/ProgressBarSwitch.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using Zenject;
 
@@ -6,20 +7,36 @@ public class ProgressBarSwitch : MonoBehaviour
     [SerializeField] private GameObject _progressBar;
 
     private StateMachine _stateMachine;
+    private MilestoneSystem _milestoneSystem;
+    private MilestonesData _milestonesData;
+
+    private bool _disposed = false;
 
     [Inject]
-    private void Construct(StateMachine stateMachine)
+    private void Construct(StateMachine stateMachine, MilestoneSystem milestoneSystem, MilestonesData milestonesData)
     {
         _stateMachine = stateMachine;
-
+        _milestoneSystem = milestoneSystem;
+        _milestonesData = milestonesData;
         _stateMachine.ChangeStateAction += OnChangeState;
+        _milestoneSystem.TheEnd += OnTheEnd;
+    }
+
+    private void OnTheEnd()
+    {
+            Debug.Log("last Milestone");
+        _disposed = true;
+            //_progressBar.SetActive(false);
     }
 
     private void OnChangeState(IExitableState state)
     {
         if (state is DecorationState || state is WorkbenchState || state is CraftState)
         {
-            _progressBar.SetActive(true);
+            if (!_disposed)
+            {
+                _progressBar.SetActive(true);
+            }
         }
         else
         {
